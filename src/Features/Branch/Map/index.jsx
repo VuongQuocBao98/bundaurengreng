bd379dc5badf2fc8fd367144406a18db4878f504
Vuong Quoc Bao <ttm68dark@gmail.com>
2023-01-12 19:32:48 +0700

    commit

--- src/Features/Branch/Map/index.jsx
+++ src/Features/Branch/Map/index.jsx
@@ -13,13 +13,20 @@ function MapData({ data }) {
   const center = { lat: data.lat, lng: data.lng };
   return (
     <div className="md:h-[20vh] lg:h-[30vh] xl:h-[40vh] 2xl:h-[50vh] h-[30vh]">
-      <GoogleMap
+      {/* <GoogleMap
         zoom={17}
         center={center}
         mapContainerClassName="map-container"
       >
         <Marker position={center} zIndex={1000} visible={true} opacity={1} />
-      </GoogleMap>
+      </GoogleMap> */}
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.200281115127!2d106.62090061535187!3d10.795967161783466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752b818050aba1%3A0xe52b9908ef6ddb55!2zQsO6biDEkOG6rXUgUmVuZyBSZW5n!5e0!3m2!1svi!2s!4v1673525645982!5m2!1svi!2s"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        className="w-full h-full"
+      ></iframe>
+      {/* <iframe src="https://platform.twitter.com/widgets/tweet_button.html"></iframe> */}
     </div>
   );
 }
