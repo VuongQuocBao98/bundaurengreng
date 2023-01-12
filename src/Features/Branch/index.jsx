a1538e13be0d2e4b58d28a7d055ee37f6fef108d
Vuong Quoc Bao <ttm68dark@gmail.com>
2023-01-12 19:27:31 +0700

    c

--- src/Features/Branch/index.jsx
+++ src/Features/Branch/index.jsx
@@ -84,6 +84,15 @@ export default function Branch() {
           </div>
         </div>
       </div>
+      {/* <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.200281115127!2d106.62090061535187!3d10.795967161783466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752b818050aba1%3A0xe52b9908ef6ddb55!2zQsO6biDEkOG6rXUgUmVuZyBSZW5n!5e0!3m2!1svi!2s!4v1673525645982!5m2!1svi!2s"
+        width="600"
+        height="450"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe> */}
       <Footer />
     </div>
   );
