54b24bb23dd97d624754d8384078beec3f7edabd
Vuong Quoc Bao <ttm68dark@gmail.com>
2023-01-12 19:24:40 +0700

    v

--- src/Constants/BranchData.js
+++ src/Constants/BranchData.js
@@ -2,10 +2,11 @@ export const BRANCH_LIST = [
   {
     id: 1,
     name: "GÒ DẦU",
-    lat: 10.796151593179756,
-    lng: 106.6231322129796,
+    lat: 10.796019864166249,
+    lng: 106.6230678423296,
     location: "146e Gò Dầu, Phường Tân Quý, Quận Tân Phú, Tp Hồ Chí Minh",
     phone: "0789.4848.66",
     time: "Giờ mở cửa: 10:00 - 22:00",
   },
 ];
+// 10.796019864166249, 106.6230678423296
